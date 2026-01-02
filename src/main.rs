f9d3b0a88bd53f4188a249a966c37c70bd2ad1cb
etu <musamuhammadetudaye@gmail.com>
2026-01-02 01:34:15 +0100

    accepting input from a user on app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, guessing game!");
+use std::io;
+
+fn main (){
+    println!("guess a number");
+    println!("input the number");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to receive input");
+
+        println!(" you guessed: {guess}")
 }
