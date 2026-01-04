6c8d381071536d143e626df4a509d5e5f69c48a8
etu <musamuhammadetudaye@gmail.com>
2026-01-04 23:26:12 +0100

    changed the congratulatory message

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
             Ordering::Less => println! ("Too small"),
             Ordering::Greater =>println!("Too  Big"),
             Ordering::Equal => {
-                println!("You win!🎊");
+                println!("You got the guess right!🎊");
                 break;
          }
             }
