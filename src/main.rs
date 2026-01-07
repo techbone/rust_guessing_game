0641691733865dd19edda3b4c0b4e4a488eef023
etu <musamuhammadetudaye@gmail.com>
2026-01-07 23:59:25 +0100

    adjusted the flow for the guess game

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-  println!("Guess a number!");
+  println!("Guess a number rn!");
 
   let secret_number = rand::thread_rng().gen_range(1..=100);
 
